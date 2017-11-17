5da559bd3adce964ebde01554f49800c33b845d9
m-murad <murad.kuka@gmail.com>
2017-11-17 23:29:49 +0530

    Added MainActivity

---
+++ app/src/main/java/com/murad/jboss/keep/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.murad.jboss.keep.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.murad.jboss.keep.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
