cb1aaefcb7ed13880b2b3d9f196f5695877cda82
m-murad <murad.kuka@gmail.com>
2017-11-17 23:19:59 +0530

    Added packages

--- app/src/main/java/com/murad/jboss/keep/activities/MainActivity.java
+++ app/src/main/java/com/murad/jboss/keep/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.murad.jboss.keep;
+package com.murad.jboss.keep.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.murad.jboss.keep.R;
 
 public class MainActivity extends AppCompatActivity {
 
