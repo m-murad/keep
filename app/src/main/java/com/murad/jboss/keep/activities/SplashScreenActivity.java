53ffe530e389905af0c2cffa32249c591a749ed7
m-murad <murad.kuka@gmail.com>
2017-11-29 08:05:35 +0530

    SplashScreeNActivity made immersive

--- app/src/main/java/com/murad/jboss/keep/activities/SplashScreenActivity.java
+++ app/src/main/java/com/murad/jboss/keep/activities/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.murad.jboss.keep.R;
 
@@ -14,12 +15,21 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     private Handler handler;
     private Runnable runnable;
+    private View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         Log.d(TAG, "onCreate");
+
+        decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         final Intent intent = new Intent(this, MainActivity.class);
 
@@ -30,7 +40,7 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         };
-        int SPLASH_DISPLAY_LENGTH = 500;
+        int SPLASH_DISPLAY_LENGTH = 1000;
         handler = new Handler();
         handler.postDelayed(runnable, SPLASH_DISPLAY_LENGTH);
     }
