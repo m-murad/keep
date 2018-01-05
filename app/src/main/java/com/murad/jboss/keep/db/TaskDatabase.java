5838f83286b78af7762d8f1b110fb97e035ae771
m-murad <murad.kuka@gmail.com>
2018-01-05 22:11:22 +0530

    Added TaskDatabase

---
+++ app/src/main/java/com/murad/jboss/keep/db/TaskDatabase.java
@@ -0,0 +1,32 @@
+package com.murad.jboss.keep.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.murad.jboss.keep.models.Task;
+
+/**
+ * Created by murad on 05/01/18.
+ */
+
+@Database(entities = {Task.class}, version = 1)
+public abstract class TaskDatabase extends RoomDatabase {
+
+    public abstract TaskDao wordDao();
+    private static TaskDatabase instance;
+
+    static TaskDatabase getDatabase(final Context context) {
+        if (instance == null) {
+            synchronized (TaskDatabase.class) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskDatabase.class, "task_database").build();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
