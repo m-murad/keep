48b65797289ba1cb6b7dee6aa058249e4374aa2c
m-murad <murad.kuka@gmail.com>
2018-01-06 02:19:19 +0530

    Added TaskViewModel

--- app/src/main/java/com/murad/jboss/keep/viewmodels/TaskViewModel.java
+++ app/src/main/java/com/murad/jboss/keep/viewmodels/TaskViewModel.java
@@ -1,8 +1,44 @@
 package com.murad.jboss.keep.viewmodels;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.murad.jboss.keep.db.TaskRepository;
+import com.murad.jboss.keep.models.Task;
+
+import java.util.List;
+
 /**
  * Created by murad on 06/01/18.
  */
 
-public class TaskViewModel {
+public class TaskViewModel extends AndroidViewModel {
+
+    private LiveData<List<Task>> tasks;
+    private TaskRepository taskRepository;
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+
+        taskRepository = new TaskRepository(application);
+        tasks = taskRepository.getAllTasks();
+    }
+
+    public LiveData<List<Task>> getTasks() {
+        return tasks;
+    }
+
+    public void insert(@NonNull Task task) {
+        taskRepository.insert(task);
+    }
+
+    public void delete(@NonNull Task task) {
+        taskRepository.delete(task);
+    }
+
+    public void update(@NonNull Task task) {
+        taskRepository.update(task);
+    }
 }
