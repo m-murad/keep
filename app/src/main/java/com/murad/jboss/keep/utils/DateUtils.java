174ba6ca3b14e2994e90cdcda26ceea6c80d7a6d
m-murad <murad.kuka@gmail.com>
2018-01-06 03:49:27 +0530

    Finalised date format

--- app/src/main/java/com/murad/jboss/keep/utils/DateUtils.java
+++ app/src/main/java/com/murad/jboss/keep/utils/DateUtils.java
@@ -1,6 +1,8 @@
 package com.murad.jboss.keep.utils;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by murad on 06/01/18.
@@ -11,6 +13,9 @@ public class DateUtils {
     public static String getDate(String dateString) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(Long.valueOf(dateString));
-        return String.valueOf(calendar.getTime());
+
+        SimpleDateFormat format = new SimpleDateFormat("d MMM, yyyy", Locale.ENGLISH);
+
+        return format.format(calendar.getTime());
     }
 }
